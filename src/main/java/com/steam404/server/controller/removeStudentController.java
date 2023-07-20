4d02bbb663777dc724279b9b79afb9a95e9b736e
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-07-20 18:02:30 +0800

    Create removeStudentController.java
    删除学生接口

---
+++ src/main/java/com/steam404/server/controller/removeStudentController.java
@@ -0,0 +1,20 @@
+//删除学生接口
+package com.steam404.server.controller;
+
+import com.steam404.server.service.removeStudentMessage;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class removeStudentController {
+    @PostMapping(value = "removeStudent")
+    public int removeStudent(
+            @RequestParam String name
+    ) throws IOException {
+        removeStudentMessage removeStudentMessage=new removeStudentMessage();
+        return removeStudentMessage.removeStudent(name);
+    }
+}
